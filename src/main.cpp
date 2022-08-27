c5adbcc5325d55e6fcab6a3f6b1ddaeb04a08e98
hide4096 <asouhide2002@gmail.com>
2022-08-27 19:58:39 +0900

    [update]UDP通信追加

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,19 @@
 #include "esp32-hal-ledc.h"
 #include "sbus.h"
 #include "MadgwickAHRS.h"
+#include "WiFi.h"
+#include "AsyncUDP.h"
 
 
 #define AUTOLED 4
+
+//デバッグ用WiFiAP
+const char *ssid = "TadpoleEx";
+const char *pass = "07033208416";
+const IPAddress ip(192,168,100,2);
+const IPAddress subnet(255,255,255,0);
+WiFiClient cli;
+AsyncUDP udp; 
 
 bfs::SbusRx sbus_rx(&Serial2);  //SBUS入力
 std::array<int16_t,bfs::SbusRx::NUM_CH()> sbus_data;
@@ -29,7 +39,7 @@ float pitch,yaw,roll;
 hw_timer_t* timer = NULL;
 
 //姿勢取得
-void IRAM_ATTR getAttitude(){
+void getAttitude(){
   mdf.updateIMU(imu.gyroX(),imu.gyroY(),imu.gyroZ(),imu.accelX(),imu.accelY(),imu.accelZ());
   roll = mdf.getPitch()*-1.0;
   yaw = mdf.getYaw()*-1.0;
@@ -87,7 +97,7 @@ void controlServo(){
 }
 
 Ticker fetcher;
-
+int cnt = 0;
 //メイン処理(1kHz)
 void fetchSensors(){
   getAttitude();
@@ -95,13 +105,6 @@ void fetchSensors(){
 
 void controlServos(void *pvParameters){
   while(1){
-  if(sbus_rx.Read()){
-    sbus_data = sbus_rx.ch();
-    for(uint8_t i = 0;i<4;i++){
-      DutyRatio[i] = sbus_data[i];
-    }
-  }
-
   //投下機構
   if(sbus_data[4] > 1024){
     if(is_CH5_reset){
@@ -127,9 +130,35 @@ void controlServos(void *pvParameters){
   }
 }
 
+void readSBUS(void *pvParameters){
+  while(1){
+    if(sbus_rx.Read()){
+    sbus_data = sbus_rx.ch();
+    for(uint8_t i = 0;i<4;i++){
+      DutyRatio[i] = sbus_data[i];
+    }
+    }
+  }
+}
+
+void sendUDP(void *pvParameters){
+  while(1){
+    char str[128];
+    sprintf(str,"%.3f,%.3f,%.3f,%d,%d,%d\r\n",pitch,yaw,roll,sbus_data[1],sbus_data[2],sbus_data[3]);
+    udp.broadcastTo(str,8901);
+    delay(1);
+  }
+}
+
 void setup() {
   Serial.begin(115200);
   sbus_rx.Begin(16,17);
+
+  /*
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid,pass);
+  if(WiFi.waitForConnectResult() != WL_CONNECTED) Serial.println("fail");
+  */
 
   for(uint8_t i = 0;i<5;i++){
     ledcSetup(i,50,16);
@@ -156,10 +185,13 @@ void setup() {
   mdf.begin(1000);
 
   fetcher.attach_ms(1,fetchSensors);
-  xTaskCreateUniversal(controlServos,"controlServos",8192,NULL,1,NULL,APP_CPU_NUM);
+  xTaskCreateUniversal(controlServos,"controlServos",4096,NULL,1,NULL,APP_CPU_NUM);
+  xTaskCreateUniversal(readSBUS,"readSBUS",1024,NULL,1,NULL,APP_CPU_NUM);
+  xTaskCreateUniversal(sendUDP,"sendUDP",1024,NULL,1,NULL,APP_CPU_NUM);
+  
 }
 
+
 void loop() {
-  Serial.printf("%f,%f,%f\r\n",pitch,yaw,roll);
-  delay(100);
+  delay(1);
 }
